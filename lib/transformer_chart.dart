f342223caeff632f3a41210d7b0513d466c4d951
HadhemiBM <benmansourhadhemi99@gmail.com>
2025-06-20 16:59:33 +0100

    up

--- lib/transformer_chart.dart
+++ lib/transformer_chart.dart
@@ -1,21 +1,18 @@
 // transformer_chart.dart
-
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
-
 class TransformerAvailabilityChart extends StatelessWidget {
   const TransformerAvailabilityChart({super.key});
-
   final Map<String, double> dataMap = const {
     "Capacité utilisée": 750,
     "Capacité libre": 250,
-  };
 
+  };
   final List<Color> colorList = const [
     Color(0xFFAEDCF2), // Capacité utilisée (bleu clair)
     Color(0xFF00C2FF), // Capacité libre (bleu vif)
+   
   ];
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -38,25 +35,21 @@ class TransformerAvailabilityChart extends StatelessWidget {
           chartType: ChartType.ring,
           baseChartColor: Colors.transparent,
 
-          colorList: colorList,
+          colorList: colorList,         
           chartValuesOptions: const ChartValuesOptions(
-            showChartValues: false,
+            showChartValues: true,
+          chartValueStyle: TextStyle(color: Colors.white),
+            
+            showChartValueBackground: false
           ),
-          totalValue: 1000,
-          // centerText: "250\nCapacité libre",
-          // centerTextStyle: const TextStyle(
-          //   fontSize: 20,
-          //   fontWeight: FontWeight.bold,
-          //   backgroundColor: Color(0xFF1A1B2F),
-          //   color: Colors.white,
-          // ),
+          totalValue: 1000,         
           legendOptions: const LegendOptions(
             legendTextStyle: TextStyle(color: Colors.white),
             showLegends: true,
+            showLegendsInRow: true,     
+            legendShape: BoxShape.rectangle,
             legendPosition: LegendPosition.top,
-          ),
-
-          
+          ),         
         ),),
         const SizedBox(height: 40),
       ],
